0a869583784075638e928be5e2b7e8fc0fb94f9a
Kosei M <miyamoto.kosei.698@s.kyushu-u.ac.jp>
2025-11-19 16:44:32 +0900

    Contents(images and sounds) are added or updated.

---
+++ ABCi/ConclusionView.swift
@@ -0,0 +1,18 @@
+//
+//  ConclusionView.swift
+//  ABCi
+//
+//  Created by Kosei Miyamoto on 2025/11/18.
+//
+
+import SwiftUI
+
+struct ConclusionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConclusionView()
+}
---
+++ ABCi/YouTubeView.swift
@@ -0,0 +1,18 @@
+//
+//  YouTubeView.swift
+//  ABCi
+//
+//  Created by Kosei Miyamoto on 2025/11/18.
+//
+
+import SwiftUI
+
+struct YouTubeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouTubeView()
+}
