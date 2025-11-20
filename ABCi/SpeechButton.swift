6f933c6f0b5cd1f2f3a7f42db5f4c9e56be7b2c3
Kosei M <miyamoto.kosei.698@s.kyushu-u.ac.jp>
2025-11-20 10:53:30 +0900

    Speech Synsesizer added.

---
+++ ABCi/SpeechButton.swift
@@ -0,0 +1,18 @@
+//
+//  SpeechButton.swift
+//  ABCi
+//
+//  Created by Kosei Miyamoto on 2025/11/20.
+//
+
+import SwiftUI
+
+struct SpeechButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeechButton()
+}
---
+++ ABCi/SpeechManager.swift
@@ -0,0 +1,8 @@
+//
+//  SpeechManager.swift
+//  ABCi
+//
+//  Created by Kosei Miyamoto on 2025/11/20.
+//
+
+import Foundation
