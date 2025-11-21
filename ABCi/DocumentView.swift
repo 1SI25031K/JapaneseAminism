1c90c0c1c4a6adf7e13cea15c8c5e381bb39f413
Kosei M <miyamoto.kosei.698@s.kyushu-u.ac.jp>
2025-11-21 09:43:39 +0900

    DocumentView, PDFView, EnView, and MicMontor added.

---
+++ ABCi/DocumentView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentView.swift
+//  ABCi
+//
+//  Created by Kosei Miyamoto on 2025/11/21.
+//
+
+import SwiftUI
+
+struct DocumentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentView()
+}
---
+++ ABCi/EnView.swift
@@ -0,0 +1,18 @@
+//
+//  EnView.swift
+//  ABCi
+//
+//  Created by Kosei Miyamoto on 2025/11/21.
+//
+
+import SwiftUI
+
+struct EnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnView()
+}
